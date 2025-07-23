288ec7a5690160913ec4a9c2e6a60780b792391c
profile picture is a shark eating a fiber optic cable <jcarpentere5291@gmail.com>
2025-07-23 14:45:10 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using RoyalJelly.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
